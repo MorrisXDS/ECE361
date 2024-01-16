4c242d4f494e08674ca23c1ab18c40e4388db84f
Xiaodong Sun <xiaodong.sun@mail.utoronto.ca>
2024-01-16 10:48:56 -0500

    [FTLP1] added header files for both parts.  server is now able to receive messages.

--- File_Transfer_Lab_Part_I/deliver.c
+++ File_Transfer_Lab_Part_I/deliver.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
+
+// This deliver is based on the assumption that the local address 
+// will always be IPv4
 int main(){
     printf("Hello World!\n");
     
