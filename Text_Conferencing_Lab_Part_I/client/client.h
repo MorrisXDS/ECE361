6043f21a44f8256eb7be945b8a82045b5fb91c85
Xiaodong Sun <xiaodong.sun@mail.utoronto.ca>
2024-03-12 01:28:50 -0400

    formated indentation and spacing for client.h

--- Text_Conferencing_Lab_Part_I/client/client.h
+++ Text_Conferencing_Lab_Part_I/client/client.h
@@ -43,21 +43,21 @@
 #define OFF 0
 #define ON 1
 
-void login(int * socket_fd, char * ip_address, char * port, char * username, char * password);
-void user_registration(int * socket_fd, char * ip_address, char * port, char * username, char * password);
-void logout(int * socket_fd);
-void join_session(int * socket_fd, char * session_id);
-void leave_session(int * socket_fd);
-void create_session(int * socket_fd, char * session_id);
-void list(int * socket_fd);
-void terminate_program(int * socket_fd);
-void promote(int *socket_fd, char* username);
-void kick(int *socket_fd, char* username);
+void login(int* socket_fd, char* ip_address, char* port, char* username, char* password);
+void user_registration(int* socket_fd, char* ip_address, char* port, char* username, char* password);
+void logout(int* socket_fd);
+void join_session(int* socket_fd, char* session_id);
+void leave_session(int* socket_fd);
+void create_session(int* socket_fd, char* session_id);
+void list(int* socket_fd);
+void terminate_program(int* socket_fd);
+void promote(int* socket_fd, char* username);
+void kick(int* socket_fd, char* username);
 void* server_message_handler(void* socket_fd);
 void set_server_connection_status(int status);
 int length_validate(int received_length, int expected_length);
 int parameter_count_validate(int received_count, int expected_count);
-void user_list(int * socket_fd, char * session_id);
+void user_list(int* socket_fd, char* session_id);
 int check_commands(char* command);
 
 #endif
